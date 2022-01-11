01e3aa2e77f387e052be9d55fb8408a852d383d8
KumarArun2349 <kumararun20610@gmail.com>
2022-01-11 10:49:58 +0530

    Second

--- src/org/inheri/Client.java
+++ src/org/inheri/Client.java
@@ -10,15 +10,20 @@ public class Client extends Company {
 		System.out.println("AABBCC");
 
 	}
-	
+	private void clientAdd() {
+		System.out.println("USA");
+
+	}
 	public static void main(String[] args) {
 		Client c1=new Client();
 		c1.clientId();
 		c1.clientName();
+		c1.clientAdd();
 		c1.cId();
 		c1.cName();
 		c1.empID();
 		c1.empName();
+		
 	}
 
 }
